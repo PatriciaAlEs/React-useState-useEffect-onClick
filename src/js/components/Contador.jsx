0fa51ef37cb960ca5ec1987e3a2882d3954af2a0
PatriciaAlEs <patriciaalvarezestevez@gmail.com>
2025-10-27 11:19:14 +0100

    Proyecto base: useState, useEffect y onClick

---
+++ src/js/components/Contador.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contador = () => {
+    return (
+        <div className="contador">
+            <h1>🧮 Contador React</h1>
+            <h2>0</h2>
+            <button>➕ Aumentar</button>
+            <button>➖ Disminuir</button>
+        </div>
+    );
+}
+
+export default Contador;
